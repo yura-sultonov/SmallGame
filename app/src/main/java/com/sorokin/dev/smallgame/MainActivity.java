9fb9e678f89400d5f22aafc5a4f8139746be528a
yura_sultonov <yura_sultonov@mail.ru>
2018-08-11 14:29:58 +0300

    written view with matrix like animation deleted .idea folder

--- app/src/main/java/com/sorokin/dev/smallgame/MainActivity.java
+++ app/src/main/java/com/sorokin/dev/smallgame/MainActivity.java
@@ -1,13 +1,38 @@
 package com.sorokin.dev.smallgame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView bestResultTextView;
+    private Button playButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+        bestResultTextView = findViewById(R.id.max_solve_value_text_view);
+        playButton = findViewById(R.id.play_button);
+        bestResultTextView.setText("2048");
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startNewGame();
+            }
+        });
+    }
+
+    private void startNewGame() {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/sorokin/dev/smallgame/MatrixEffectView.java
@@ -0,0 +1,82 @@
+package com.sorokin.dev.smallgame;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+import java.util.Random;
+
+public class MatrixEffectView extends View {
+
+    private static final Random RANDOM = new Random();
+    private int width, height;
+    private Canvas canvas;
+    private Bitmap canvasBmp;
+    private int fontSize = 40;
+    private char[] cars = "1234567890".toCharArray();
+    private int[] txtPosByColumn;
+    private Paint paintTxt, paintBg, paintBgBmp, paintInitBg;
+
+    public MatrixEffectView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        paintTxt = new Paint();
+        paintTxt.setStyle(Paint.Style.FILL);
+        paintTxt.setColor(Color.GREEN);
+        paintTxt.setTextSize(fontSize);
+        paintBg = new Paint();
+        paintBg.setColor(Color.BLACK);
+        paintBg.setAlpha(5);
+        paintBg.setStyle(Paint.Style.FILL);
+        paintBgBmp = new Paint();
+        paintBgBmp.setColor(Color.BLACK);
+        paintInitBg = new Paint();
+        paintInitBg.setColor(Color.BLACK);
+        paintInitBg.setAlpha(255);
+        paintInitBg.setStyle(Paint.Style.FILL);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        width = w;
+        height = h;
+        canvasBmp = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        canvas = new Canvas(canvasBmp);
+        canvas.drawRect(0, 0, width, height, paintInitBg);
+        int columnSize = width / fontSize;
+        txtPosByColumn = new int[columnSize + 1];
+
+        for (int x = 0; x < columnSize; x++) {
+            txtPosByColumn[x] = RANDOM.nextInt(height / 2) + 1;
+        }
+    }
+
+    private void drawText() {
+        for (int i = 0; i < txtPosByColumn.length; i++) {
+            canvas.drawText("" + cars[RANDOM.nextInt(cars.length)], i * fontSize, txtPosByColumn[i] * fontSize, paintTxt);
+
+            if (txtPosByColumn[i] * fontSize > height && Math.random() > 0.975) {
+                txtPosByColumn[i] = 0;
+            }
+
+            txtPosByColumn[i]++;
+        }
+    }
+
+    private void drawCanvas() {
+        canvas.drawRect(0, 0, width, height, paintBg);
+        drawText();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawBitmap(canvasBmp, 0, 0, paintBgBmp);
+        drawCanvas();
+        invalidate();
+    }
+
+}
