e5f19387eb8f7e754b84e7f53bcd49ec0eccb1c6
Goncharoff <nik.ignition.work@gmail.com>
2018-08-11 18:20:01 +0300

    MUSICCC

--- app/src/main/java/com/sorokin/dev/smallgame/GameActivity.java
+++ app/src/main/java/com/sorokin/dev/smallgame/GameActivity.java
@@ -1,24 +1,61 @@
 package com.sorokin.dev.smallgame;
 
 import android.app.Activity;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 
 public class GameActivity extends Activity {
 
     GameView gameView;
 
+    private MyLooper mLooper = new MyLooper();;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        gameView = findViewById(R.id.gameView);
+
+
+        mLooper.start();
+
+
     }
+
 
     @Override
     protected void onResume() {
         super.onResume();
-        gameView.resume();
 
+
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+
+    }
+    private class MyLooper extends Thread {
+        public Handler mHandler;
+
+        public void run() {
+
+            while (true) {
+                Music music = new Music();
+                try {
+                    music.playTrack(music.musicToPlay());
+                    Thread.sleep(5320L);
+
+                } catch (InterruptedException ex) {
+                    //Treat the exception
+                }
+            }
+        }
     }
 
 }
