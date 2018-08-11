9017ea6050c191bd71b80a30272a9f43d9354515
sorokinDev <sorokin.dev@mail.ru>
2018-08-11 17:56:16 +0300

    App is playing now

--- app/src/main/java/com/sorokin/dev/smallgame/GameActivity.java
+++ app/src/main/java/com/sorokin/dev/smallgame/GameActivity.java
@@ -2,22 +2,49 @@ package com.sorokin.dev.smallgame;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class GameActivity extends Activity {
 
+    Timer tmr;
+    class MyTimerTask extends TimerTask {
+
+        @Override
+        public void run() {
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    tvScores.setText("Scores: " + gameView.scores);
+                    if(gameView.isFail){
+
+                    }
+                }
+            });
+        }
+    }
+
     GameView gameView;
+    TextView tvScores;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
         gameView = findViewById(R.id.gameView);
+        tvScores = findViewById(R.id.tvScores);
+
+        tmr = new Timer();
+        tmr.scheduleAtFixedRate(new MyTimerTask(), 0, 500);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        gameView.resume();
+        //gameView.resume();
 
     }
 
--- app/src/main/java/com/sorokin/dev/smallgame/GameView.java
+++ app/src/main/java/com/sorokin/dev/smallgame/GameView.java
@@ -1,68 +1,353 @@
 package com.sorokin.dev.smallgame;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.PixelFormat;
+import android.graphics.PorterDuff;
+import android.graphics.Rect;
 import android.graphics.RectF;
+import android.text.TextPaint;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class GameView extends SurfaceView implements Runnable {
+class GameView extends SurfaceView implements SurfaceHolder.Callback {
+    int[] numbers = new int[]{
+            2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096
+    };
 
-    int fWidth = 5;
-    int fHeight = 0;
+    int[] colors = new int[]{
+        Color.GREEN, 0xFF01FFFF, 0xFFFFFF01, 0xFFFF7F00, 0xFFE34166, 0xFFFFD800, Color.GREEN, 0xFF01FFFF, 0xFFFFFF01, 0xFFFF7F00, 0xFFE34166, 0xFFFFD800
+    };
+
+    int fWidth = 4;
+    int fHeight = 5;
 
     int lineWidth = 0;
-    int cubePadding = 8;
+    int cubePadding = 16;
+    int cubeStrokeWidth = 20;
+    int textSize = 80;
 
-    volatile boolean playing;
-    Thread gameThread = null;
+    int startingMax = 3;
+
+    int frameNum = 0;
+
+    boolean isFail = false;
 
     Paint cubePaint;
+    TextPaint textPaint;
     Paint paint;
-    Canvas canvas;
-    SurfaceHolder surfaceHolder;
 
-    List<Cube> cubes;
+    int[][] gameField;
+
+    int updateGap = 25;
+
+    private DrawThread drawThread;
+
+    int scores = 0;
+
+    @SuppressLint("ClickableViewAccessibility")
+    public void init(){
+        gameField = new int[fWidth][fHeight];
+        for(int i = 0; i < fWidth; ++i){
+            for(int j = 0; j < fHeight; ++j){
+                gameField[i][j] = 0;
+            }
+        }
+        paint = new Paint();
+        cubePaint = new Paint();
+        cubePaint.setStyle(Paint.Style.STROKE);
+        cubePaint.setStrokeWidth(cubeStrokeWidth);
+
+        textPaint = new TextPaint();
+        textPaint.setTextSize(textSize);
+        textPaint.setStyle(Paint.Style.FILL);
+        textPaint.setAntiAlias(true);
+
+        setBackgroundColor(Color.TRANSPARENT);
+        setZOrderOnTop(true); //necessary
+        getHolder().setFormat(PixelFormat.TRANSPARENT);
+        getHolder().addCallback(this);
+        this.setOnTouchListener(new OnSwipeTouchListener(this.getContext()) {
+
+            @Override
+            public void onSwipeLeft(View view) {
+                super.onSwipeLeft(view);
+                Log.e("SWIPE", "LEFT");
+                for(int x = 1; x < fWidth; ++x){
+                    for(int y = fHeight - 1; y >= 0; --y){
+                        if(gameField[x][y] == 0){
+                            continue;
+                        }
+                        if(gameField[x-1][y] == 0){
+                            gameField[x-1][y] = gameField[x][y];
+                            gameField[x][y] = 0;
+                        }else if(gameField[x-1][y] == gameField[x][y]){
+                            gameField[x-1][y] *= 2;
+                            scores += gameField[x][y];
+                            gameField[x][y] = 0;
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onSwipeRight(View view) {
+                super.onSwipeRight(view);
+                for(int x = fWidth - 2; x >= 0; --x){
+                    for(int y = fHeight - 1; y >= 0; --y){
+                        if(gameField[x][y] == 0){
+                            continue;
+                        }
+                        if(gameField[x+1][y] == 0){
+                            gameField[x+1][y] = gameField[x][y];
+                            gameField[x][y] = 0;
+                        }else if(gameField[x+1][y] == gameField[x][y]){
+                            gameField[x+1][y] *= 2;
+                            scores += gameField[x][y];
+                            gameField[x][y] = 0;
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    public void update(){
+
+        int flyCount = 0;
+
+        for(int x = 0; x < fWidth; ++x){
+            int y = fHeight - 2;
+            while(y >= 0){
+                if(gameField[x][y+1] == 0){
+                    gameField[x][y+1] = gameField[x][y];
+                    gameField[x][y] = 0;
+                }
+                --y;
+            }
+        }
+
+        for(int x = 0; x < fWidth; ++x) {
+            int y = fHeight - 1;
+            while (y > 0) {
+                if(gameField[x][y-1] == gameField[x][y]){
+                    scores += gameField[x][y];
+                    gameField[x][y] *= 2;
+                    gameField[x][y-1] = 0;
+                }
+                y--;
+            }
+        }
+
+        for(int x = 0; x < fWidth; ++x){
+            int flyingStack = 0;
+            for(int y = 0; y < fHeight; ++y) {
+                if (gameField[x][y] != 0) {
+                    flyingStack++;
+                }
+                if(y == fHeight - 1) {
+                    flyingStack = 0;
+                }else if(gameField[x][y] == 0){
+                    flyCount += flyingStack;
+                    flyingStack = 0;
+                }
+            }
+            flyCount += flyingStack;
+        }
+
+
+        boolean generateNewCube;
+        if(flyCount == 1){
+            generateNewCube = Math.random() > 0.85;
+        }else generateNewCube = flyCount == 0;
+
+        if(generateNewCube){
+            int newCubeX;
+            int newCubeValue = (int)(Math.random() * startingMax);
+            newCubeX = (int)(Math.random() * fWidth);
+            gameField[newCubeX][0] = numbers[newCubeValue];
+        }
+
+        boolean isFail = true;
+        for(int x = 0; x < fWidth; ++x){
+            if(gameField[x][0] == 0){
+                isFail = false;
+            }
+        }
+        if(isFail){
+            Log.e("FAIL", "IS FAIL");
+            isFail = false;
+        }
+    }
+
+    public void drawGame(Canvas canvas) {
+        canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
+
+        cubePaint.setColor(Color.RED);
+        for (int x = 0; x < fWidth; ++x) {
+            for (int y = 0; y < fHeight; ++y) {
+                if(gameField[x][y] != 0){
+                    for(int j = 0; j < numbers.length; ++j){
+                        if(numbers[j] == gameField[x][y]){
+                            cubePaint.setColor(colors[j]);
+                            //cubePaint.setShadowLayer(20, 0, 0, colors[j]);
+                            textPaint.setColor(colors[j]);
+                            //textPaint.setShadowLayer(20, 0, 0, colors[j]);
+
+                            break;
+                        }
+                    }
+                    Rect textBounds = new Rect();
+                    String text = String.valueOf(gameField[x][y]);
+                    textPaint.getTextBounds(text, 0, text.length(), textBounds);
+
+
+                    RectF cubeRect = cubeToRect(x, y);
+                    canvas.drawRoundRect(cubeRect, 8f, 8f, cubePaint);
+
+                    RectF textRect = cubeRect;
+                    textRect.left = cubeRect.left + cubeRect.width() / 2 - textBounds.width() / 2;
+                    textRect.right = cubeRect.left + cubeRect.width() / 2 + textBounds.width() / 2;
+
+                    textRect.top = cubeRect.top + cubeRect.height() / 2 - ((textPaint.descent() + textPaint.ascent()) / 2);
+                    textRect.bottom = cubeRect.top + cubeRect.height() / 2 + textBounds.height() / 2;
+                    canvas.drawText(text, textRect.left, textRect.top, textPaint);
+
+                }
+            }
+        }
+
+    }
+
+    public void control(){
+
+    }
+
+    public RectF cubeToRect(int x, int y){
+        RectF rect = new RectF();
+
+        int offsetX = 0;
+        int offsetY = 0;
+        if(y != fHeight - 1){
+            offsetY = (int)((1.0 * frameNum / updateGap) * lineWidth);
+        }
+        if(y == fHeight - 1){
+            offsetY = 0;
+        }else{
+            boolean shouldOffset = false;
+            for(int i = y + 1; i < fHeight; ++i){
+                if(gameField[x][i] == 0){
+                    shouldOffset = true;
+                }
+            }
+            if(!shouldOffset){
+                offsetY = 0;
+            }
+        }
+
+        rect.left = (x * lineWidth) + cubePadding + offsetX;
+        rect.right = ((x + 1) * lineWidth) - cubePadding + offsetX;
+
+        rect.top = (y * lineWidth) + cubePadding + offsetY;
+        rect.bottom = ((y + 1) * lineWidth) - cubePadding + offsetY;
+
+        return rect;
+    }
+
+    class DrawThread extends Thread {
+        private boolean running = false;
+        private SurfaceHolder surfaceHolder;
+
+        public DrawThread(SurfaceHolder surfaceHolder) {
+            this.surfaceHolder = surfaceHolder;
+        }
+
+        public void setRunning(boolean running) {
+            this.running = running;
+        }
+
+        @Override
+        public void run() {
+            Canvas canvas;
+            while (running) {
+                canvas = null;
+                try {
+                    canvas = surfaceHolder.lockCanvas(null);
+                    if (canvas == null)
+                        continue;
+
+                    frameNum++;
+                    frameNum = frameNum % updateGap;
+                    if(frameNum == 0) {
+                        update();
+                    }
+
+                    drawGame(canvas);
+
+                    control();
+
+                } finally {
+                    if (canvas != null) {
+                        surfaceHolder.unlockCanvasAndPost(canvas);
+                    }
+                }
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
     public GameView(Context context) {
         super(context);
-
-        cubes = new ArrayList<>();
-
-        surfaceHolder = getHolder();
-        paint = new Paint();
-
-        cubePaint = new Paint();
-        cubePaint.setStyle(Paint.Style.FILL_AND_STROKE);
-
+        init();
     }
 
     public GameView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        cubes = new ArrayList<>();
-
-        surfaceHolder = getHolder();
-        paint = new Paint();
-
-        cubePaint = new Paint();
-        cubePaint.setStyle(Paint.Style.FILL_AND_STROKE);
+        init();
     }
 
-    public GameView(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-        cubes = new ArrayList<>();
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width,
+                               int height) {
 
-        surfaceHolder = getHolder();
-        paint = new Paint();
+    }
 
-        cubePaint = new Paint();
-        cubePaint.setStyle(Paint.Style.FILL_AND_STROKE);
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        drawThread = new DrawThread(getHolder());
+        drawThread.setRunning(true);
+        drawThread.start();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        boolean retry = true;
+        drawThread.setRunning(false);
+        while (retry) {
+            try {
+                drawThread.join();
+                retry = false;
+            } catch (InterruptedException e) {
+            }
+        }
     }
 
     @Override
@@ -85,96 +370,97 @@ public class GameView extends SurfaceView implements Runnable {
         lineWidth = this.getMeasuredWidth() / fWidth;
     }
 
-    @Override
-    public void run() {
-        while (playing) {
-            update();
-            draw();
-            control();
+
+    public class OnSwipeTouchListener implements OnTouchListener {
+
+        public
+        OnSwipeTouchListener(Context ctx)
+        {
+            m_gestureDetector = new GestureDetector(ctx, new GestureListener());
         }
-    }
 
-    private void update() {
-        int flyCount = 0;
-        for(int i = 0; i < cubes.size(); i++){
-            Cube cube = cubes.get(i);
-            if(cube.isFly){
-                cube.y++;
+        public boolean
+        onTouch(final View view, final MotionEvent motionEvent)
+        {
+            m_view = view;
+            return m_gestureDetector.onTouchEvent(motionEvent);
+        }
 
-                if(cube.y == fHeight){
-                    cube.isFly = false;
+        private final class GestureListener extends GestureDetector.SimpleOnGestureListener {
+
+            private static final int SWIPE_THRESHOLD = 100;
+            private static final int SWIPE_VELOCITY_THRESHOLD = 100;
+
+            @Override
+            public boolean
+            onDown(MotionEvent e)
+            {
+                return true;
+            }
+
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                boolean result = false;
+                try {
+                    float diffY = e2.getY() - e1.getY();
+                    float diffX = e2.getX() - e1.getX();
+                    if (Math.abs(diffX) > Math.abs(diffY)) {
+                        if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                            if (diffX > 0) {
+                                onSwipeRight(m_view);
+                            } else {
+                                onSwipeLeft(m_view);
+                            }
+                        }
+                        result = true;
+                    }
+                    else if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                        if (diffY > 0) {
+                            onSwipeBottom(m_view);
+                        } else {
+                            onSwipeTop(m_view);
+                        }
+                    }
+                    result = true;
+
+                } catch (Exception exception) {
+                    exception.printStackTrace();
                 }
-
-            }
-
-            if(cube.isFly){
-                ++flyCount;
+                return result;
             }
         }
 
-        boolean generateNewCube;
-        if(flyCount == 1){
-            if(Math.random() > 0.7){
-                generateNewCube = true;
-            }else{
-                generateNewCube = false;
-            }
-        }else if(flyCount == 0){
-            generateNewCube = true;
-        }else{
-            generateNewCube = false;
+        /**
+         * @brief Override to process swipe-right event
+         */
+        public void onSwipeRight(View view)
+        {
         }
 
-        if(generateNewCube){
-            cubes.add(new Cube(fWidth));
-
+        /**
+         * @brief Override to process swipe-left event
+         */
+        public void onSwipeLeft(View view)
+        {
         }
-    }
 
-    private void draw() {
-        if (surfaceHolder.getSurface().isValid()) {
-            canvas = surfaceHolder.lockCanvas();
-            canvas.drawColor(Color.BLACK);
-
-            cubePaint.setColor(Color.RED);
-
-            for (Cube cube: cubes) {
-                canvas.drawRoundRect(cubeToRect(cube), 8f, 8f, paint);
-            }
-
-            surfaceHolder.unlockCanvasAndPost(canvas);
+        /**
+         * @brief Override to process swipe-top event
+         */
+        public void onSwipeTop(View view)
+        {
         }
-    }
 
-    private void control() {
-        try {
-            gameThread.sleep(300);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        /**
+         * @brief Override to process swipe-bottom event
+         */
+        public void onSwipeBottom(View view)
+        {
         }
+
+        /////////////////////////////////////////////////////////////////////////////
+        private final GestureDetector m_gestureDetector;
+        private View m_view;
     }
 
-    public void pause() {
-        playing = false;
-        try {
-            gameThread.join();
-        } catch (InterruptedException e) {
-        }
-    }
-
-    public void resume() {
-        playing = true;
-        gameThread = new Thread(this);
-        gameThread.start();
-    }
-
-    public RectF cubeToRect(Cube cube){
-        RectF rect = new RectF();
-        rect.left = (cube.x * lineWidth) + cubePadding;
-        rect.right = ((cube.x + 1) * lineWidth) - cubePadding;
-        rect.top = (cube.y * lineWidth) + cubePadding;
-        rect.bottom = ((cube.y + 1) * lineWidth) - cubePadding;
-
-        return rect;
-    }
 }
